9f67a458b4f807578f6f01e2a8cafab526846bfe
Michael Miklis <info@michaelmiklis.de>
2019-11-16 14:00:07 +0100

    initial commit

---
+++ Download-Ignite2019Slides.ps1
@@ -0,0 +1,48 @@
+######################################################################
+## (C) 2019 Michael Miklis (michaelmiklis.de)
+##
+##
+##
+## Version:       1.0
+##
+## Release:       Final
+##
+## Requirements:  -none-
+##
+## Description:   Downloads all Microsoft Ignite 2019 Slides
+##
+## This script is provided 'AS-IS'.  The author does not provide
+## any guarantee or warranty, stated or implied.  Use at your own
+## risk. You are free to reproduce, copy & modify the code, but
+## please give the author credit.
+##
+####################################################################
+
+param (
+    [parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()][string]$downloadPath,
+    [parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()][int]$ParallelJobs,
+    [parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()][switch]$All
+)
+
+# Get all sessions using myIgnite API
+$Sessions = (Invoke-RestMethod 'https://api-myignite.techcommunity.microsoft.com/api/session/all') | Where-Object { $_.slideDeck } | Select-Object sessionCode, title, slideDeck -first 10
+
+# If not -All is passed, open Out-GridView
+if (!$all)
+{
+    $Sessions = $Sessions | Out-GridView -PassThru
+}
+
+# Use parallel ForEach in PowerShell 7
+if ($PSVersionTable.PSVersion.Major -ge 7)
+{
+    $Sessions | ForEach-Object -Parallel { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.title); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $using:downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.title)); } -ThrottleLimit $ParallelJobs 
+}
+
+else 
+{
+    $Sessions | ForEach-Object { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.title); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.title)); }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Download-Ignite2019Slides
- Download script for all Microsoft Ignite 2019 Slides
+# Download-Ignite2019Slides.ps1
+Download all or just selected slides, which were persented at the Microsoft Ignite 2019 conference in Orlando:
+ - Script support parallel processing, when executed in PowerShell 7
+ - Script also supports PowerShell version 6
+ - Use "-All" parameter to download all availabled slides
+ - Use GridView UI to select individual sessions to download
+
+
+## Parameters
+The PowerShell script can be executed using the following parameters:
+
+| Parameter | Sample-Value | Description |
+| -------------| -----| ----- | 
+| -downloadPath |  "C:\Users\michael\Document\Ignite-Slides" |  Path, where to store the downloaded slides| 
+| -ParallelJobs|  10 |  Start 10 simultaneous download sessions (only supported in PowerShell 7 and above)| 
+| -all | | When specified, all slides from all sessions will be downloaded, otherwise a GridView for selecting the sessions will be displayed | 
+
+## Examples
+The following example downloads all available session to the folder "C:\Ignite-Slides" using 5 simultaneous download sessions (requires PowerShell 7.0)
+
+```
+./Download-Ignite2019Slides.ps1 -downloadPath "C:\Ignite-Slides" -ParallelJobs 5 -all
+```
