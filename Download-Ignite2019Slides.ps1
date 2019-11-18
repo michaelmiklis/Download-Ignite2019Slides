af0eccff15aa6fdf4a5670b3377cabd7773a4917
Michael Miklis <info@michaelmiklis.de>
2019-11-18 18:35:57 +0100

    Update Download-Ignite2019Slides.ps1
    Fixed illegal characters

--- Download-Ignite2019Slides.ps1
+++ Download-Ignite2019Slides.ps1
@@ -29,7 +29,7 @@ param (
 )
 
 # Get all sessions using myIgnite API
-$Sessions = (Invoke-RestMethod 'https://api-myignite.techcommunity.microsoft.com/api/session/all') | Where-Object { $_.slideDeck } | Select-Object sessionCode, title, slideDeck
+[array]$Sessions = (Invoke-RestMethod 'https://api-myignite.techcommunity.microsoft.com/api/session/all') | Where-Object { $_.slideDeck } | Select-Object sessionCode, title, slideDeck
 
 # If not -All is passed, open Out-GridView
 if (!$all)
@@ -37,13 +37,21 @@ if (!$all)
     $Sessions = $Sessions | Out-GridView -PassThru
 }
 
+for ($i = 0; $i -le $($Sessions.Count - 1); $i++)
+{
+    $slideFilename = $Sessions[$i].title
+    [System.IO.Path]::GetInvalidFileNameChars() | ForEach-Object { $slideFilename = $slideFilename.replace("$_","") }
+    Add-Member -InputObject $Sessions[$i] -MemberType NoteProperty -Name "slideFilename" -Value $slideFilename
+
+}
+
 # Use parallel ForEach in PowerShell 7
 if ($PSVersionTable.PSVersion.Major -ge 7)
 {
-    $Sessions | ForEach-Object -Parallel { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.title); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $using:downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.title)); } -ThrottleLimit $ParallelJobs 
+    $Sessions | ForEach-Object -Parallel { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.slideFilename); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $using:downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.slideFilename)); } -ThrottleLimit $ParallelJobs 
 }
 
 else 
 {
-    $Sessions | ForEach-Object { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.title); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.title)); }
+    $Sessions | ForEach-Object { Write-Host $("Processing {0} - {1}" -f $_.sessionCode, $_.slideFilename); Invoke-WebRequest -Uri $_.slideDeck -OutFile $(Join-Path $downloadPath $("{0}_{1}.pptx" -f $_.sessionCode, $_.slideFilename)); }
 }
